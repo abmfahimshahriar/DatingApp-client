70c7af095a568fa469f4d2ba4121b0a63e89e24d
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-01 16:55:16 +0600

    add members service

---
+++ src/app/members/services/members.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Member} from "../../models/Member";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    })
+  }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users',this.httpOptions);
+  }
+
+  getMember(username) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username,this.httpOptions);
+  }
+}
