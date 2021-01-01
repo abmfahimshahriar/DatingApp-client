b37c77dd52086b8f8e00e61899b2c19b9ba9107e
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-02 03:40:20 +0600

    cache members data using member service

--- src/app/members/services/members.service.ts
+++ src/app/members/services/members.service.ts
@@ -2,22 +2,33 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient } from "@angular/common/http";
 import {Member} from "../../models/Member";
+import {of} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(
     private http: HttpClient,
   ) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if(this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if(member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
