9e0e55fe788f0a6322402ba1db8d31e82fbb4ad1
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-06 01:09:24 +0600

    refactor member service

--- src/app/members/components/member-list/member-list.component.ts
+++ src/app/members/components/member-list/member-list.component.ts
@@ -3,6 +3,10 @@ import {Member} from "../../../models/Member";
 import {MembersService} from "../../services/members.service";
 import {Observable} from "rxjs";
 import {Pagination} from "../../../models/pagination";
+import {UserParams} from "../../../models/UserParams";
+import {User} from "../../../models/user";
+import {AccountService} from "../../../core/services/account.service";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-member-list',
@@ -13,26 +17,32 @@ export class MemberListComponent implements OnInit {
 
   members: Member[];
   pagination: Pagination;
-  pageNumber = 1;
-  pageSize = 5;
+  userParams: UserParams;
+  user: User;
 
   constructor(
     private memberService: MembersService,
-  ) { }
+    private accountService: AccountService,
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+  }
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.pageNumber,this.pageSize).subscribe(response => {
+    this.memberService.getMembers(this.userParams).subscribe(response => {
       this.members = response.result;
       this.pagination = response.pagination;
     });
   }
 
   pageChanged(event: any) {
-    this.pageNumber = event.page;
+    this.userParams.pageNumber = event.page;
     this.loadMembers();
   }
 
