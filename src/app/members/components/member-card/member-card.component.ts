5dbe4c280b7e71e512c12359856eae78d6cd77e0
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-07 04:06:49 +0600

    add like a member feature

--- src/app/members/components/member-card/member-card.component.ts
+++ src/app/members/components/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Member} from "../../../models/Member";
+import {MembersService} from "../../services/members.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-member-card',
@@ -10,9 +12,19 @@ export class MemberCardComponent implements OnInit {
 
   @Input() member: Member;
   noPhotoUrl = "https://res.cloudinary.com/fshahriar008/image/upload/v1609701702/user_bccush.png";
-  constructor() { }
+
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  addLike(member: Member) {
+    console.log('like clicked');
+    this.memberService.addLike(member.username).subscribe(() => {
+      this.toastr.success('You have liked' + member.knownAs);
+    })
+  }
 }
