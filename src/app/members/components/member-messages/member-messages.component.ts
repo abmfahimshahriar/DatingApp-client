c8836ac7f98f2ee36c7e4f099433b4a50233dfbf
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-14 08:34:26 +0600

    UI fix in message component

--- src/app/members/components/member-messages/member-messages.component.ts
+++ src/app/members/components/member-messages/member-messages.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../../../models/message";
 import {MessageService} from "../../../shared/services/message.service";
 import {NoPhotoUrl} from "../../../shared/utility/noImagePhoto";
 import {NgForm} from "@angular/forms";
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.scss']
